eafa5723c573eb20e535334c9593fb7886b33db6
Sandra <126724691+dorkyWolfie@users.noreply.github.com>
2023-11-17 10:59:41 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # national-parks-macedonia
-A website about the nacional parks in Republic of North Macedonia
+
+This is a website that contains all the important information about the national parks in my home country North Macedonia, while conserving good looks, easy access and great functionality.
+It's written in HTML, styled with CSS and i gave it a bit of functionality with JavaScript. This project is in the group of firsts, so don't judge the code, i am still a newbie.
+
+This project is the thesis for my BA in graphic design. So the copyright is shared with the AUE - FON Univerity and my great mentor prof. d-r Marija Veteroska.
---
+++ dropdown.js
@@ -0,0 +1,169 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("drop-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function myFunctionF() {
+  document.getElementById("myDropdownF").classList.toggle("showF");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn-flag')) {
+    var dropdownsF = document.getElementsByClassName("drop-content-flag");
+    var i;
+    for (i = 0; i < dropdownsF.length; i++) {
+      var openDropdownF = dropdownsF[i];
+      if (openDropdownF.classList.contains('showF')) {
+        openDropdownF.classList.remove('showF');
+      }
+    }
+  }
+}
+
+// skrieno meni
+function openBurger() {
+  document.getElementById("menu").style.display = "flex";
+}
+
+function closeBurger() {
+  document.getElementById("menu").style.display = "none";
+}
+
+// aktivnosti pop-up
+function openPopup1() {
+  document.getElementById("popup-1").style.display = "block";
+}
+
+function closePopup1() {
+  document.getElementById("popup-1").style.display = "none";
+}
+
+function openPopup2() {
+  document.getElementById("popup-2").style.display = "block";
+}
+
+function closePopup2() {
+  document.getElementById("popup-2").style.display = "none";
+}
+
+function openPopup3() {
+  document.getElementById("popup-3").style.display = "block";
+}
+
+function closePopup3() {
+  document.getElementById("popup-3").style.display = "none";
+}
+
+function openPopup4() {
+  document.getElementById("popup-4").style.display = "block";
+}
+
+function closePopup4() {
+  document.getElementById("popup-4").style.display = "none";
+}
+
+function openPopup5() {
+  document.getElementById("popup-5").style.display = "block";
+}
+
+function closePopup5() {
+  document.getElementById("popup-5").style.display = "none";
+}
+
+function openPopup6() {
+  document.getElementById("popup-6").style.display = "block";
+}
+
+function closePopup6() {
+  document.getElementById("popup-6").style.display = "none";
+}
+
+function openPopup7() {
+  document.getElementById("popup-7").style.display = "block";
+}
+
+function closePopup7() {
+  document.getElementById("popup-7").style.display = "none";
+}
+
+function openPopup8() {
+  document.getElementById("popup-8").style.display = "block";
+}
+
+function closePopup8() {
+  document.getElementById("popup-8").style.display = "none";
+}
+
+function openPopup9() {
+  document.getElementById("popup-9").style.display = "block";
+}
+
+function closePopup9() {
+  document.getElementById("popup-9").style.display = "none";
+}
+
+function openPopup10() {
+  document.getElementById("popup-10").style.display = "block";
+}
+
+function closePopup10() {
+  document.getElementById("popup-10").style.display = "none";
+}
+
+function openPopup11() {
+  document.getElementById("popup-11").style.display = "block";
+}
+
+function closePopup11() {
+  document.getElementById("popup-11").style.display = "none";
+}
+
+function openPopup12() {
+  document.getElementById("popup-12").style.display = "block";
+}
+
+function closePopup12() {
+  document.getElementById("popup-12").style.display = "none";
+}
+
+function openPopup13() {
+  document.getElementById("popup-13").style.display = "block";
+}
+
+function closePopup13() {
+  document.getElementById("popup-13").style.display = "none";
+}
+
+function openPopup14() {
+  document.getElementById("popup-14").style.display = "block";
+}
+
+function closePopup14() {
+  document.getElementById("popup-14").style.display = "none";
+}
+
+function scrolTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function openBlock(blockId) {
+  var initialText = document.getElementById('start');
+    initialText.style.display = 'none';
+  var blocks = document.querySelectorAll('.hide');
+  blocks.forEach(function(block) {
+      block.style.display = 'none';
+  });
+  var selectedBlock = document.getElementById(blockId);
+  selectedBlock.style.display = 'flex';
+}
