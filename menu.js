5452d782fb8e0bc2842bddae709053b669edbfc8
Sandra <126724691+dorkyWolfie@users.noreply.github.com>
2024-01-19 11:10:13 +0100

    Add changes to the style

---
+++ menu.js
@@ -0,0 +1,16 @@
+// OPEN MOBILE MENU
+function openMenu() {
+    document.getElementById("mobMenu").style.display = "flex";
+}
+
+// CLOSE MOBILE MENU
+function closeMenu() {
+    document.getElementById("mobMenu").style.display = "none";
+}
+
+// function to scroll to top when icon is clicked
+function scrolTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
---
+++ popup.js
@@ -0,0 +1,113 @@
+// AKTIVNOSTI POPUP
+function openPopup1() { // NURKANJE
+    document.getElementById("popup-1").style.display = "block";
+}
+
+function closePopup1() {
+    document.getElementById("popup-1").style.display = "none";
+}
+
+function openPopup2() { // PLIVANJE
+    document.getElementById("popup-2").style.display = "block";
+}
+
+function closePopup2() {
+    document.getElementById("popup-2").style.display = "none";
+}
+
+function openPopup3() { // PLANINSKI VELOSIPEDIZAM
+    document.getElementById("popup-3").style.display = "block";
+}
+
+function closePopup3() {
+  document.getElementById("popup-3").style.display = "none";
+}
+
+function openPopup4() { // KAJAKARENJE
+  document.getElementById("popup-4").style.display = "block";
+}
+
+function closePopup4() {
+  document.getElementById("popup-4").style.display = "none";
+}
+
+function openPopup5() { // NABLJUDUVANJE PTICI
+  document.getElementById("popup-5").style.display = "block";
+}
+
+function closePopup5() {
+  document.getElementById("popup-5").style.display = "none";
+}
+
+function openPopup6() { // PLANINARENJE
+  document.getElementById("popup-6").style.display = "block";
+}
+
+function closePopup6() {
+  document.getElementById("popup-6").style.display = "none";
+}
+
+function openPopup7() { // RIBOLOV
+  document.getElementById("popup-7").style.display = "block";
+}
+
+function closePopup7() {
+  document.getElementById("popup-7").style.display = "none";
+}
+
+function openPopup8() { // PARAGLAJDING
+  document.getElementById("popup-8").style.display = "block";
+}
+
+function closePopup8() {
+  document.getElementById("popup-8").style.display = "none";
+}
+
+function openPopup9() { // SKIJANJE
+  document.getElementById("popup-9").style.display = "block";
+}
+
+function closePopup9() {
+  document.getElementById("popup-9").style.display = "none";
+}
+
+function openPopup10() { // JAVANJE KONJ
+  document.getElementById("popup-10").style.display = "block";
+}
+
+function closePopup10() {
+  document.getElementById("popup-10").style.display = "none";
+}
+
+// PRESTOJ POPUP
+function openPopup11() { // HOTEL
+  document.getElementById("popup-11").style.display = "block";
+}
+
+function closePopup11() {
+  document.getElementById("popup-11").style.display = "none";
+}
+
+function openPopup12() { // APARTMAN
+  document.getElementById("popup-12").style.display = "block";
+}
+
+function closePopup12() {
+  document.getElementById("popup-12").style.display = "none";
+}
+
+function openPopup13() { // PLANINSKI DOM
+  document.getElementById("popup-13").style.display = "block";
+}
+
+function closePopup13() {
+  document.getElementById("popup-13").style.display = "none";
+}
+
+function openPopup14() { // KAMPUVANJE
+  document.getElementById("popup-14").style.display = "block";
+}
+
+function closePopup14() {
+  document.getElementById("popup-14").style.display = "none";
+}
